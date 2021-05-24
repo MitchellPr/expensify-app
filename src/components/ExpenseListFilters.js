b6da2106cdf8d2621e8ea3c6dea5da0d72c11d8b
Mitchell <mitchellprince90@gmail.com>
2021-05-24 15:10:53 +0200

    Setup production build and server

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,54 +1,64 @@
-import React from 'react'
-import { DateRangePicker } from 'react-dates'
-import { connect } from 'react-redux'
-import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../actions/filters'
+import React from 'react';
+import { connect } from 'react-redux';
+import { DateRangePicker } from 'react-dates';
+import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../actions/filters';
 
-class ExpenseListFilters extends React.Component {
+export class ExpenseListFilters extends React.Component {
   state = {
     calendarFocused: null
-  }
+  };
   onDatesChange = ({ startDate, endDate }) => {
-    this.props.setStartDate(startDate)
-    this.props.setEndDate(endDate)
-  }
+    this.props.setStartDate(startDate);
+    this.props.setEndDate(endDate);
+  };
   onFocusChange = (calendarFocused) => {
-    this.setState(() => ({ calendarFocused }))
+    this.setState(() => ({ calendarFocused }));
   }
-  onTextChange = ((e) => {
-    this.props.setTextFilter(e.target.value)
-  })
-  onSortChange = ((e) => {
+  onTextChange = (e) => {
+    this.props.setTextFilter(e.target.value);
+  };
+  onSortChange = (e) => {
     if (e.target.value === 'date') {
-      this.props.sortByDate()
+      this.props.sortByDate();
     } else if (e.target.value === 'amount') {
-      this.propssortByAmount()
+      this.props.sortByAmount();
     }
-  })
-
+  };
   render() {
     return (
       <div>
-          <input type="text" value={this.props.filters.text} onChange={this.onTextChange}/>
-          <select value={this.props.filters.sortBy} onChange={onSortChange}>
-            <option value="date">Date</option>
-            <option value="amount">Amount</option>
-          </select>
-          <DateRangePicker 
-            startDate={this.props.filters.startDate}
-            endDate={this.props.filters.endDate}
-            onDatesChange={this.onDatesChange}
-            focusedInput={this.state.calendarFocused}
-            onFocusChange={this.onFocusChange}
-            showClearDates={true}
-            numberOfMonths={1}
-            isOutsideRange={() => false}
-          />
-        </div>
-    )
+        <input
+          type="text"
+          value={this.props.filters.text}
+          onChange={this.onTextChange}
+        />
+        <select
+          value={this.props.filters.sortBy}
+          onChange={this.onSortChange}
+        >
+          <option value="date">Date</option>
+          <option value="amount">Amount</option>
+        </select>
+        <DateRangePicker
+          startDateId={"dwjkhqkehwqjkeq"}
+          endDateId={"cxzvcxbzbczxbz"}
+          // startDate={this.props.filters.startDate}
+          // endDate={this.props.filters.endDate}
+          onDatesChange={this.onDatesChange}
+          focusedInput={this.state.calendarFocused}
+          onFocusChange={this.onFocusChange}
+          showClearDates={true}
+          numberOfMonths={1}
+          isOutsideRange={() => false}
+        />
+      </div>
+    );
   }
-}
+};
 
-const mapStateToProps = (state) => ({filters: state.filters})
+const mapStateToProps = (state) => ({
+  filters: state.filters
+});
 
 const mapDispatchToProps = (dispatch) => ({
   setTextFilter: (text) => dispatch(setTextFilter(text)),
@@ -56,7 +66,6 @@ const mapDispatchToProps = (dispatch) => ({
   sortByAmount: () => dispatch(sortByAmount()),
   setStartDate: (startDate) => dispatch(setStartDate(startDate)),
   setEndDate: (endDate) => dispatch(setEndDate(endDate))
-})
+});
 
- 
-export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters)
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters);
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,60 @@
 const path = require('path')
-
-// console.log(__dirname);
-
-module.exports = {
-  target: 'web',
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: "babel-loader",
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'eval-cheap-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    liveReload: true,
-    historyApiFallback: true
-  }
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+ 
+module.exports = (env) =>{
+    const isProduction = (env.production === true);
+ 
+    console.log(env);
+    return {
+        entry:'./src/app.js',
+    output: {
+        path:path.join(__dirname,'public'),
+        filename: 'bundle.js'
+    },
+    mode:'development',
+    module: {
+        rules:[{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env','@babel/preset-react']
+                }
+            }
+            
+        },
+        {
+            test: /\.s?css$/,
+            use: [
+                    {
+                        loader: isProduction ? MiniCssExtractPlugin.loader : 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ] 
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+          filename: '/styles.css'
+        })
+    ],
+    devtool: isProduction ?'source-map':'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname,'public'),
+        historyApiFallback: true,
+        publicPath: '/'
+    }
+    }
 }
-
