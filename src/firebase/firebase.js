dd025129e5c6642111b1b27d5fbc618a14237e4b
Mitchell <mitchellprince90@gmail.com>
2021-05-28 11:18:09 +0200

    Webpack error, trying to resolve 2

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,7 +11,7 @@ import 'firebase/database';
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    // appId: process.env.FIREBASE_APP_ID
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
