c65c7a62e4ddaab59ee06de438dd120d1f86ac1d
Mitchell <mitchellprince90@gmail.com>
2021-05-28 11:12:52 +0200

    Webpack error, trying to resolve

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 import 'firebase/auth'
 import 'firebase/database';
-import DotEnv from 'dotenv'
+
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
