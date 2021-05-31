6ed8532eec6b003bd24adf3c84c88819b487deb3
Mitchell <mitchellprince90@gmail.com>
2021-05-31 15:37:23 +0200

    Add private routes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -18,8 +18,9 @@ import 'firebase/database';
  
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
 
-export { firebase, database as default }
+export { firebase, googleAuthProvider, database as default }
 
 
 
