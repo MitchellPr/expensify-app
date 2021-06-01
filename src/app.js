316494002742be66dfbe20a9179d8bcdd6b215f8
Mitchell <mitchellprince90@gmail.com>
2021-06-01 15:08:04 +0200

    Trying to resolve the linking issue 3

--- src/app.js
+++ src/app.js
@@ -35,16 +35,17 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     store.dispatch(login(user.uid))
-  store.dispatch(startSetExpenses()).then(() => {
-    renderApp()
-    if (history.location.pathname === '/') {
-      history.push('/dashboard')
-    }
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp()
+      // if (history.location.pathname === '/') {
+      //   history.push('/dashboard')
+      // }
 })
     
   } else {
     store.dispatch(logout())
     renderApp()
-    history.push('/')
+    // history.push('/')
   }
 })
+
